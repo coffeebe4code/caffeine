d2c37bb2da7606bc03cb53f3d29247c5ac45e359
Chris <chris.clark@coffeeb4code.com>
2021-08-03 09:56:44 -0500

    added server threading

--- src/server/server.c
+++ src/server/server.c
@@ -3,26 +3,25 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
 
 static struct sockaddr_in sa;
 static int socket_fd;
-static pthread_t server_id;
-static int * clients;
+static pthread_t *server_ids;
+static int max_conns = SOMAXCONN;
+static int *client_fds;
 
-void * handle_controller() {
-  return NULL;
-}
+void *handle_controller() { return NULL; }
 
 void server_init() {
   signal(SIGTERM, server_shutdown);
-  
+  signal(SIGCHLD, SIG_IGN);
   socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
   if (socket_fd == -1) {
@@ -53,41 +52,54 @@ void server_init() {
     exit(EXIT_FAILURE);
   }
 
-  if (listen(socket_fd, 10) == -1) {
+  if (listen(socket_fd, SOMAXCONN) == -1) {
     perror("listen failed");
     close(socket_fd);
     exit(EXIT_FAILURE);
   }
+  client_fds = (int *)calloc(max_conns, sizeof(int *));
 }
 
 void server_construct() {}
 
-void * server_loop() {
+void *server_loop(void *client_id) {
   for (;;) {
-    int connect_fd = accept(socket_fd, NULL, NULL);
-
-    if (connect_fd == -1) {
+    int id = accept(socket_fd, NULL, NULL);
+    client_fds[*(int *)client_id] = id;
+    if (id == -1) {
       if (errno != EWOULDBLOCK) {
         perror("accept failed");
         close(socket_fd);
         exit(EXIT_FAILURE);
       }
+    } else {
     }
-    else {
-    }
-    close(connect_fd);
+    id = close(id);
   }
   pthread_exit(NULL);
 }
 
 void server_run() {
-  pthread_create(&server_id, NULL, &server_loop, NULL);
-  pthread_join(server_id, NULL);
+  server_ids = (pthread_t *)malloc(max_conns * sizeof(pthread_t *));
+
+  for (int i = 0; i < (max_conns); i++) {
+    pthread_create(&server_ids[i], NULL, &server_loop, (void *)(&i));
+  }
+  for (int i = 0; i < (max_conns); i++) {
+    pthread_join(server_ids[i], NULL);
+  }
 }
 
 void server_shutdown() {
   shutdown(socket_fd, SHUT_WR);
-  pthread_cancel(server_id);
+  for (int i = 0; i < max_conns; i++) {
+    pthread_cancel(server_ids[i]);
+    if (client_fds[i] != 0 || client_fds[i] != -1) {
+      close(client_fds[i]);
+    }
+  }
+  free(server_ids);
+  free(client_fds);
   shutdown(socket_fd, SHUT_RD);
   close(socket_fd);
   exit(EXIT_SUCCESS);
