fde6adf1758ac19503a523302a73b22e84d66ca2
Chris <chris.clark@coffeeb4code.com>
2021-09-11 15:25:01 -0500

    nearly got the exec part

--- src/barista/barista.c
+++ src/barista/barista.c
@@ -1,41 +1,60 @@
 #include "./barista.h"
-#include <string.h>
 #include "stdlib.h"
+#include <string.h>
 
-METHOD * methods;
-char ** routes;
-typedef responder_t (*FUNCS)(requester_t *, const char *, const int);
-FUNCS * execs;
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+METHOD *methods;
+char **routes;
+typedef responder_t (*FUNCS)(header_t);
+FUNCS *execs;
 int len = 0;
 int cap = 25;
 
-void barista_add(METHOD method, char * route, responder_t (* func)(requester_t, const char *, const int)) {
-  if(len == 0) {
+void barista_add(METHOD method, char *route,
+                 responder_t (*func)(header_t)) {
+  if (len == 0) {
     methods = malloc(sizeof(METHOD) * cap);
     routes = malloc(sizeof(const char *) * cap);
     execs = malloc(sizeof((*func)) * cap);
   }
-  if(cap == len) {
+  if (cap == len) {
     cap *= 2;
     methods = realloc(methods, sizeof(METHOD) * cap);
     routes = realloc((void *)routes, sizeof(const char *) * cap);
     execs = realloc(execs, sizeof(FUNCS));
   }
   methods[len] = method;
-  char * ptr = malloc(sizeof(char) * strlen(route));
+  char *ptr = malloc(sizeof(char) * strlen(route));
   strcpy(ptr, route);
   routes[len] = ptr;
-
   execs[len] = func;
-
   len++;
 }
 
 void barista_free() {
   free(methods);
   free(execs);
-  for(int i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++) {
     free(routes[i]);
   }
   free(routes);
+}
+
+responder_t barista_exec(const int index, const char *buffer,
+                         const int buffer_len) {
+  header_go(index, buffer, buffer_len);
+  header_t header = header_get(index);
+  for (int i = 0; i < len; i++) {
+    if (likely(header.method == methods[i])) {
+      int tocheck = header.route_end - header.route_start;
+      const char *head_buf = header.header;
+      int equal = memcmp(&head_buf[header.route_start], routes[i], tocheck);
+      if (likely(equal)) {
+        return execs[i](header);
+      }
+    }
+  }
+  return NULL;
 }
--- src/barista/barista.h
+++ src/barista/barista.h
@@ -3,7 +3,7 @@
 #include "../requester/requester.h"
 #include "../responder/responder.h"
 
-void barista_add(METHOD method, char * route, responder_t (* func)(requester_t, const char *, const int));
+void barista_add(METHOD method, char * route, responder_t (* func)(header_t));
 void barista_free();
-responder_t barista_exec(const char * buffer, const int buffer_len);
+responder_t barista_exec(const int index, const char * buffer, const int buffer_len);
 
