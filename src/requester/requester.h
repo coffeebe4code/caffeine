e4c6c2c7e0d315fb00e5a5bdd366197758fe1782
Chris <chris.clark@coffeeb4code.com>
2021-09-25 12:29:19 -0500

    renamed header to requester

--- src/requester/requester.h
+++ src/requester/requester.h
@@ -1,4 +1,28 @@
 #pragma once
 
-typedef struct requester {
+typedef enum METHOD {
+  GET,
+  POST,
+  PUT,
+  DELETE,
+  OPTIONS,
+  PATCH,
+  UNSUPPORTED
+} METHOD;
+
+typedef struct requester_t {
+  int index;
+  int route_start;
+  int route_end;
+  const char * header;
+  int buffer_len;
+  enum METHOD method;
 } requester_t;
+
+void requester_init(const int total_possible);
+requester_t requester_get(int index);
+void requester_reset(const int index);
+void requester_go(int index, const char * buffer, const int buffer_len);
+void parse_route(int index);
+void parse_method(int index);
+void requester_free();
