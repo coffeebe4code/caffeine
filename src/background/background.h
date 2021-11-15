6aecbe706da727d0e77c319937a45fc4ce563871
Chris <chris.clark@coffeeb4code.com>
2021-11-14 21:24:37 -0600

    switching contexts

--- src/background/background.h
+++ src/background/background.h
@@ -1,7 +1,5 @@
 #pragma once
 
-
-
 void background_init();
 void background_add_spin(int milliseconds, void * raw_data, void (*func)(void *)); 
 void background_add_mutex(int milliseconds, void * raw_data, void (*func)(void *)); 
